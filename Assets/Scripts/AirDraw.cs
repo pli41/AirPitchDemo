506de72c1c96f1f6393b3cd43ee8ccf9005b7b7e
Peilin Li <lipeilin@gatech.edu>
2016-10-31 23:41:09 -0400

    Initial push
    separate trails implemented

---
+++ Assets/Scripts/AirDraw.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class AirDraw : MonoBehaviour {
+
+    [SerializeField]
+    GameObject trail;
+    GameObject currentTrail;
+
+    [SerializeField]
+    Transform savedTrailsObj;
+    List<TrailRenderer> savedTrails;
+
+
+
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine("InputListener");
+        savedTrails = new List<TrailRenderer>();
+        
+
+	}
+	
+
+
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+
+    void StartDraw()
+    {
+        currentTrail = Instantiate(trail, transform.position, transform.rotation, transform) as GameObject;
+    }
+
+    void EndDraw()
+    {
+        savedTrails.Add(currentTrail.GetComponent<TrailRenderer>());
+        currentTrail.transform.SetParent(savedTrailsObj);
+
+    }
+
+    
+
+
+    IEnumerator InputListener()
+    {
+        while (true)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Debug.Log("123");
+                StartDraw();
+            }
+            else if (Input.GetKeyUp(KeyCode.Space))
+            {
+                EndDraw();
+            }
+
+            yield return null;
+        }
+        
+    }
+
+}
